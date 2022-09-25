6bca72226ac79497ae720d1d04ecf60a57b7e508
AidenFlowers <CoffehBear@protonmail.com>
2022-09-24 23:00:59 -0400

    getting stuck

---
+++ Unity_Foundations_All_Scenes/Assets/_Project/Scripts/EnergyProjectile.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.TestTools;
+using System.Collections;
+
+
+    public class EnergyProjectile : MonoBehaviour
+    {
+        public GameObject hitParticlesPrefab;
+        public float maxLifetime = 10f;
+        float timer;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(collision.gameObject.CompareTag("Enemy"))
+            {
+                Instantiate(hitParticlesPrefab, transform.position, transform.rotation);
+                Destroy(gameObject);
+            }
+        }
+
+        void Start(){
+            timer = 0f;
+        }
+
+        void Update()
+        {
+            timer += Time.deltaTime;
+            if(timer > maxLifetime)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+
